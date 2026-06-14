6341fa37f67fcccf49d1fbb4ee8f7a71438df3f6
haroon-mirza <amirzaharoon@gmail.com>
2026-06-14 15:36:56 -0400

    add dbt staging model and data tests

--- dbt_supply_chain_warning/models/staging/stg_fred_observations.sql
+++ dbt_supply_chain_warning/models/staging/stg_fred_observations.sql
@@ -3,4 +3,4 @@ select
     cast(date as date) as observation_date,
     cast(nullif(value, '.') as numeric) as indicator_value
 from {{ source('raw', 'bronze_fred_observations') }}
-where value is not null
+where nullif(value, '.') is not null
